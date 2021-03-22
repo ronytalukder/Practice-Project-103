b0bec657c193e870c257bcfe8a5400fdf277d232
unknown <you@example.com>
2021-03-22 19:12:10 +0600

    done

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 14, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("day").innerHTML = days
+    document.getElementById("Hours").innerHTML = hours
+    document.getElementById("Miss").innerHTML = minutes
+    document.getElementById("Secs").innerHTML = seconds
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+//test slider js==================start=================
+$('.test_slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fas fa-angle-left priv"></i>',
+    nextArrow: '<i class="fas fa-angle-right next"></i>',
+
+});
+//test slider js==================end=================
